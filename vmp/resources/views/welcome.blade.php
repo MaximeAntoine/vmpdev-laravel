56674441b6b78fa33f379affdcef7b5021b89601
Maxime Antoine <maxime05.antoine@gmail.com>
2018-07-10 11:25:57 +0200

    no message

--- vmp/resources/views/welcome.blade.php
+++ vmp/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    VMP
                 </div>
 
                 <div class="links">
